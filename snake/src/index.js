c08f5301279b84915bc8e597b840300da3f3af68
Ryan Brooks <rbrook1598@gmail.com>
2021-09-11 16:16:33 -0600

    randomly generating food item for snake

--- snake/src/index.js
+++ snake/src/index.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
 
+const GAME_WIDTH = 1600;
+const GAME_HEIGHT = 900;
+
+canvas.width = GAME_WIDTH;
+canvas.height = GAME_HEIGHT;
 
 ctx.fillStyle = '#ffffff';
-ctx.fillRect(20, 20, 10, 10);
+ctx.fillRect(10 * Math.floor(Math.random() * (canvas.width / 10)), 10 * Math.floor(Math.random() * (canvas.height / 10)), 10, 10);
